6dc8ea7541c5224e6f900e07a437682f7e918c64
yyue <yy@yyue.dev>
2022-04-12 11:51:21 +0000

    add: crpto

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AES, lib } from 'crypto-js';
+import {
+  AES, enc, lib, mode, pad,
+} from 'crypto-js';
 import { Message } from 'google-protobuf';
+import { decodeStringToUint8Array, encodeByteArray } from 'src/app/utils/google-closure-library';
 
 export interface Deserialize<T extends Message = Message> {
   deserializeBinary(bytes: Uint8Array): T;
@@ -11,19 +14,20 @@ export interface Deserialize<T extends Message = Message> {
 })
 
 export class CryptoService {
-  protected key = '2a4ac92b8217a77a';
+  protected key = enc.Utf8.parse('2a4ac92b8217a77a');
 
-  setKey(key:string) {
-    this.key = key;
-  }
+  protected iv = enc.Utf8.parse('44bf0314c4e1b101');
 
   encAES<T extends Message>(
     message: T | string,
   ) {
-    console.log('enc', message, (message as any).serializeBinary(), Array.from((message as any).serializeBinary()));
     const value = message instanceof Message
-      ? lib.WordArray.create(Array.from(message.serializeBinary())) : message;
-    return AES.encrypt(value, this.key).toString();
+      ? encodeByteArray(message.serializeBinary()) : message;
+    return AES.encrypt(value, this.key, {
+      iv: this.iv,
+      mode: mode.CBC,
+      padding: pad.Pkcs7,
+    }).toString();
   }
 
   dncAES(message: string):string;
@@ -32,13 +36,12 @@ export class CryptoService {
     message: string,
     decode?: Deserialize<T>,
   ) {
-    const dnc = AES.decrypt(message, this.key);
+    const dnc = AES.decrypt(message, this.key, { iv: this.iv, mode: mode.CBC, padding: pad.Pkcs7 })
+      .toString(enc.Utf8);
     if (!decode) {
-      return dnc.toString();
+      return dnc;
     }
-    const arr = new Uint8Array(dnc.words);
-    const res = decode.deserializeBinary(arr);
-    console.log('dnc', res, arr, dnc.words);
+    const res = decode.deserializeBinary(decodeStringToUint8Array(dnc));
     return res;
   }
 }
